af29ad47f8dca44f63ec8483e1485b98b9f05ea8
Gautam <gauti783@gmail.com>
2021-12-21 20:08:44 +0530

    added controller for user and admin

--- spring-security/src/main/java/com/example/springsecurity/controller/AppController.java
+++ spring-security/src/main/java/com/example/springsecurity/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,15 @@ public class AppController {
     @GetMapping
     public String getWelcomeMessage(){
         return "Welcome to Spring Security Demo";
+    }
+
+    @GetMapping("/user/{name}")
+    public String getUserMessage(@PathVariable String name){
+        return "Welcome to Spring Security Demo" + name;
+    }
+
+    @GetMapping("/admin/{name}")
+    public String getAdminMessage(@PathVariable String name){
+        return "Welcome to Spring Security Demo" + name;
     }
 }
