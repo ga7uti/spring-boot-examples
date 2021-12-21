ce581b01ef47fdb944f30c430bd07065fa7f4428
Gautam <gauti783@gmail.com>
2021-12-21 19:46:46 +0530

    refactored code

--- spring-boot-scopes/src/main/java/com/example/scopes/SpringScopesProjectApplication.java
+++ spring-boot-scopes/src/main/java/com/example/scopes/SpringScopesProjectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.support.AbstractApplicationContext;
 
 @SpringBootApplication
 public class SpringScopesProjectApplication {
