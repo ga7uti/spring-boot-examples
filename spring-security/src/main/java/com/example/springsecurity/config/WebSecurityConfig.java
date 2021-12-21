6e833b379181a42ccd16089e33a03e34d9c03cdc
Gautam <gauti783@gmail.com>
2021-12-21 20:06:45 +0530

    added in memory authentication

---
+++ spring-security/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("gauti").password("{noop}gauti").authorities("USER");
+    }
+
+}
